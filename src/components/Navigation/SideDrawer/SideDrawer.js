fc94e123fe2cb5e2774f20d7172bfc348314b951
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-02-05 13:45:49 -0500

    working with react router dom in order to  send to different links and re-render components depending on state and links

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -11,7 +11,7 @@ const sideDrawer = (props) => {
 	}
 	return (
 		<Auxillary>
-			<Backdrop show={props.open} clicked={props.closed} />
+			<Backdrop show={!props.open} clicked={props.closed} />
 			<div className={attachedClasses.join(" ")}>
 				<div className={classes.Logo}>
 					<Logo />
