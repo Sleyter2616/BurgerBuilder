a6b5e49869f001eddbbeddca83ae67d29c8f0ddc
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-02-10 22:17:48 -0500

    created user sign in and sign up route using API Key on fire base, Added error message when invalid inputs are entered. Used provided csrf token in order to log in securely, logs out after 1 hour

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ const navigationItems = (props) => (
 			BurgerBuilder
 		</NavigationItem>
 		<NavigationItem link="/orders">Orders</NavigationItem>
+		<NavigationItem link="/auth">Authenticate</NavigationItem>
 	</ul>
 );
 export default navigationItems;
