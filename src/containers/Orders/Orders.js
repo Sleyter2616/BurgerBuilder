0fbffe669238430194097bc95a7cca2429e229f8
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-02-11 12:52:09 -0500

    made it so users have their own place to fetch/filter from on firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -7,7 +7,7 @@ import {connect} from "react-redux";
 import Spinner from "../../components/UI/Spinner/Spinner";
 class Orders extends Component {
 	componentDidMount() {
-		this.props.onFetchOrders(this.props.token);
+		this.props.onFetchOrders(this.props.token, this.props.userId);
 	}
 	render() {
 		let orders = <Spinner />;
@@ -29,11 +29,13 @@ const mapStateToProps = (state) => {
 		loading: state.order.loading,
 		orders: state.order.orders,
 		token: state.auth.token,
+		userId: state.auth.userId,
 	};
 };
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onFetchOrders: (token) => dispatch(actions.fetchOrders(token)),
+		onFetchOrders: (token, userId) =>
+			dispatch(actions.fetchOrders(token, userId)),
 	};
 };
 
